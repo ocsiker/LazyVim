472dd829c742fc6eb45bb7cceb5e4062591d15b0
ocsiker <qthang89vn@gmail.com>
2024-12-16 11:15:35 +0700

    feat: change keymap from default to enter

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     { "saghen/blink.compat", opts = { impersonate_nvim_cmp = true } },
     { "saadparwaiz1/cmp_luasnip" },
-    { dependencies = "L3MON4D3/LuaSnip" },
+    { "L3MON4D3/LuaSnip" },
   },
   opts = {
     sources = {
@@ -27,6 +27,23 @@ return {
       jump = function(direction)
         require("luasnip").jump(direction)
       end,
+    },
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+
+    keymap = { preset = "enter" },
+    --appearance
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
     },
   },
 }
