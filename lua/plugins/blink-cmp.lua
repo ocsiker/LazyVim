57e0c17cf43eb1f63d2a1e10a46fb0d5724aa322
ocsiker <qthang89vn@gmail.com>
2024-12-20 08:12:34 +0700

    fix:complete with emoji

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -7,47 +7,47 @@ return {
     { "L3MON4D3/LuaSnip" },
     { "codeium.nvim" },
     { "hrsh7th/cmp-emoji" },
-    opts = {
-      sources = {
-        completion = { enabled_providers = { "lsp", "path", "luasnip", "buffer", "emoji" } },
-        providers = {
-          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-          codeium = { kind = "Codeium", score_offset = 100, async = true },
-          emoji = { name = "emoji", module = "blink.compat.source" },
-        },
-        compat = { "luasnip", "codeium", "emoji" },
+  },
+  opts = {
+    sources = {
+      completion = { enabled_providers = { "lsp", "path", "luasnip", "buffer", "emoji" } },
+      providers = {
+        dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        codeium = { kind = "Codeium", score_offset = 100, async = true },
+        emoji = { name = "emoji", module = "blink.compat.source" },
       },
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
-      },
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- see the "default configuration" section below for full documentation on how to define
-      -- your own keymap.
+      compat = { "luasnip", "codeium", "emoji" },
+    },
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
 
-      keymap = { preset = "enter" },
-      --appearance
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
+    keymap = { preset = "enter" },
+    --appearance
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
     },
   },
 }
