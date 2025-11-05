89479d52baab8ad3267e4a90cfd931ae2cdd4f72
ocsiker <qthang89vn@gmail.com>
2025-11-05 09:14:06 +0700

    feat:map g/ to change current dir of file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
+vim.keymap.set("n", "g/", function()
+  local dir = vim.fn.expand("%:p:h") -- Lấy đường dẫn thư mục của file hiện tại (an toàn hơn %:p:h trong cmd)
+  if dir == "" then
+    vim.notify("No file open, cannot change directory.", vim.log.levels.WARN)
+    return
+  end
+  vim.cmd("lcd " .. vim.fn.fnameescape(dir)) -- Chạy lcd, escape path nếu cần (an toàn với ký tự đặc biệt)
+  vim.notify("Changed to directory: " .. dir, vim.log.levels.INFO) -- Hiển thị thông báo
+end, { desc = "change to dir of current file" })
 
 vim.keymap.set("n", "<leader>fk", function()
   require("adoc_search").search_adoc_keywords()
